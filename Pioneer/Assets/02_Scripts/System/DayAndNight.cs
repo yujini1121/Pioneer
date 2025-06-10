614f63744e68599e610301b51d713c8ef5318aa5
yujini <yujini1121@naver.com>
2025-06-10 22:06:59 +0900

    씬 전환 구현 + 낮밤 전환 구현

---
+++ Pioneer/Assets/02_Scripts/System/DayAndNight.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class DayAndNight : MonoBehaviour
+{
+    public Volume volume;
+    private ColorAdjustments colorAdjustments;
+
+    public Gradient dayToNight;
+    public Gradient nightToDay;
+    public AnimationCurve exposureByTime;
+    public float dayDuration = 60f; // 하루 시간 (초)
+
+    private float timer;
+
+    void Start()
+    {
+        volume.profile.TryGet(out colorAdjustments);
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        float t = (timer % dayDuration) / dayDuration;
+
+        colorAdjustments.colorFilter.value = dayToNight.Evaluate(t);
+        colorAdjustments.postExposure.value = exposureByTime.Evaluate(t);
+    }
+}
