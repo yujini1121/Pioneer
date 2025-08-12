560ee41b76dfee2311f167eaa48868f8ffadfc10
yujini1121 <yujini1121@naver.com>
2025-08-12 18:10:32 +0900

    발리스타 수정중

--- Pioneer/Assets/02_Scripts/Common/Object/TBallista.cs
+++ Pioneer/Assets/02_Scripts/Common/Object/TBallista.cs
@@ -12,22 +12,19 @@ public class TBallista : StructureBase
     [SerializeField] private float attackSpeed;
     [SerializeField] private float attackCooltime;
 
+    [SerializeField] private float rotationSpeed;
+
 
     // TODO: 추후, ScriptableObject의 변수들과 연동 필요함~~~
     private int currentHP = 80;
     private float attackDamage = 25f;
 
-    #region
-    //public override void Start()
-    //{
-        
-    //}
 
+    // 메서드 시작
     void Update()
     {
         
     }
-    #endregion
 
     void Use()
     {
@@ -43,12 +40,12 @@ public class TBallista : StructureBase
 
     void LookAt()
     {
-        
+        // 바라보게 하는 로직
     }
     
     void Fire()
     {
-
+        // 화살 오브젝트 생성 및 발사하는 로직
     }
 
 
