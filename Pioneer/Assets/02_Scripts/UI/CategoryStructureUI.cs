d0fa21bb9fdb7b87d36934e6cac954ec0059e77f
chocokart2 <chocokart@naver.com>
2025-08-15 12:13:08 +0900

    ㅁㅁㅁ

--- Pioneer/Assets/02_Scripts/UI/CategoryStructureUI.cs
+++ Pioneer/Assets/02_Scripts/UI/CategoryStructureUI.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CategoryStructureUI : MonoBehaviour
 {
     //[Header("UI")]
+    public Button button;
 
 
 }
