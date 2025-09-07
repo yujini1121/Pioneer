586d355a57e6ecb8786a5e3f3b6b90b329e7ab80
PChaebin <127742686+PChaebin@users.noreply.github.com>
2025-09-07 20:12:41 +0900

    25.09.07 (2)
    - 포만감 기능 추가
    - 음식 섭취시 함수 구현 남음
    - 스테이터스 레벨 구현 남음
    - 정신력 구현 남음

--- Pioneer/Assets/02_Scripts/Common/Player/PlayerCore.cs
+++ Pioneer/Assets/02_Scripts/Common/Player/PlayerCore.cs
@@ -19,17 +19,39 @@ float attackRange = 0.4f;			// 공격 거리
 - 포만감
 - 정신력
 - 체력 깎이는 함수 + 체력 올라가는 함수
+=============================================================================================
+25.09.07 남은 일
+    - 음식 섭취 했을 때 어떻게 구현할 것인지
+    - 정신력 구현
+    - 스테이터스 레벨 구현
  */
 
 public class PlayerCore : CreatureBase, IBegin
 {
     // 생체 시스템 변수
-    int fullness;       // 포만감 변수    
-    int maxFullness = 100;
-    float fullnessTimer = 0f;
-    float fullnessDecrease = 5f;
 
-    int mental;         // 정신력 변수
+        // 포만감 열거형 (fullness 변수 값에 따른 상태)
+    public enum FullnessState
+    {
+        Full,       // 배부름 (80 ~ 100)
+        Normal,     // 보통 (30 ~ 79)
+        Hungry,     // 배고픔 (1 ~ 29)
+        Starving    // 굶주림 (0)
+    }
+    
+        // 포만감 변수  
+    int fullness;         
+    int maxFullness = 100;
+    FullnessState currentFullnessState;
+    int fullnessStarvingMax = 100;
+    private Coroutine starvationCoroutine;
+
+    [Header("포만감 설정")]
+    [SerializeField] private float fullnessDecreaseTime = 5f;    // 포만감 기본 감소 속도(시간)
+    [SerializeField] private float fullnessModifier = 1.3f;      // 포만감 감소 속도 증가값 => 30%
+
+        // 정신력 변수
+    int mental;         
     int maxMental = 100;
 
     [Header("공격 설정")]
@@ -40,10 +62,17 @@ public class PlayerCore : CreatureBase, IBegin
     private Rigidbody playerRb;
     private bool isAttacking = false;
 
+    private float defaultSpeed;
+
     void Awake()
     {
         playerRb = GetComponent<Rigidbody>();
         SetSetAttribute();
+    }
+    
+    void Start()
+    {
+        StartCoroutine(FullnessSystemCoroutine());
     }
 
     void Update()
@@ -57,9 +86,10 @@ public class PlayerCore : CreatureBase, IBegin
     void SetSetAttribute()
     {
         maxHp = 100;
-        hp = maxHp;                   // 체력
+        hp = maxHp;                 // 체력
         speed = 4.0f;               // 이동 속도
-        fullness = maxFullness;     // 포만감
+        defaultSpeed = speed;
+        fullness = 80;              // 포만감
         mental = maxMental;         // 정신력
         attackDamage = 2;           // 공격력
         attackDelayTime = 0.4f;     // 공격 쿨타임
@@ -116,8 +146,106 @@ public class PlayerCore : CreatureBase, IBegin
 
     // =============================================================
     // 포만감
+    // - 시작시 80으로 설정, 최대 100 최소 0
+    // - 현실 시간 5초에 한 번씩 1씩 감소
+    // - 플레이어 체력이 50% 미만이면 감소 속도 30% 증가 
+        // - 100 ~ 80 배부름 상태 : 속도 20% 증가
+        // - 79 ~ 30 배부름 상태 해제
+        // - 29 ~ 1 배고픔 상태 : 속도 30% 감소
+        // - 0 굶주림 상태 : 체력이 초 당 1씩 감소 (최대 100초)
+    // - 음식 종류에 따라 최소 5 ~ 80까지 증가 가능
+        // - 음식 종류가 무엇인지 알아야 할 듯?
+    //====================================
+    // 25.09.07 : 포만감 굶주림 코루틴 수정
     // =============================================================
+    
+    // 초당 포만감 1씩 감소 Start 함수에서 시작 (코루틴)
+    private IEnumerator FullnessSystemCoroutine()
+    {
+        while(true)
+        {
+            float currentDecreaseTime = fullnessDecreaseTime;
+            if (hp < maxHp * 0.5f)
+            {
+                currentDecreaseTime = fullnessDecreaseTime / fullnessModifier;
+            }
 
+            yield return new WaitForSeconds(currentDecreaseTime);
+
+            if(fullness > 0)
+            {
+                fullness--;
+                UpdateFullnessState();
+            }
+            Debug.Log($"굶주림 수치 : {fullness}");
+        }
+    }
+
+    /// <summary>
+    /// 포만감 수치에 따라 상태 갱신 함수
+    /// </summary>
+    private void UpdateFullnessState()
+    {
+        FullnessState fullnessState;
+
+        if (fullness >= 80)
+            fullnessState = FullnessState.Full;
+        else if (fullness >= 30)
+            fullnessState = FullnessState.Normal;
+        else if (fullness >= 1)
+            fullnessState = FullnessState.Hungry;
+        else
+            fullnessState = FullnessState.Starving;
+
+        if(fullnessState != currentFullnessState)
+        {
+            currentFullnessState = fullnessState;
+
+            switch(currentFullnessState)
+            {
+                case FullnessState.Full:
+                    speed = defaultSpeed * 1.2f;
+                    break;
+                case FullnessState.Hungry:
+                    speed = defaultSpeed * 0.7f;
+                    break;
+                default:
+                    speed = defaultSpeed;
+                    break;
+            }
+
+            if(currentFullnessState == FullnessState.Starving)      // 굶주림 상태일때
+            {
+                if(starvationCoroutine == null)
+                    starvationCoroutine = StartCoroutine(StarvingDamageCorountine());
+            }
+            else                                                    // 굶주림 상태가 아닐때
+            {
+                if (starvationCoroutine != null)
+                {
+                    StopCoroutine(starvationCoroutine);
+                    starvationCoroutine = null;
+                }
+            }
+        }
+    }
+
+    // 초당 체력 1씩 감소하는 굶주림 함수 (코루틴)
+    private IEnumerator StarvingDamageCorountine()
+    {        
+        Debug.Log("굶주림 상태 : 체력 감소 시작");
+        for(int i = 0; i < fullnessStarvingMax; i++)
+        {
+            yield return new WaitForSeconds(1f);
+            TakeDamage(1, this.gameObject);
+        }        
+    }
+
+    // public 형의 음식 섭취시 호출할 수 있는 함수 추가
+    public void EatFood()
+    {
+
+    }
 
     // =============================================================
     // 정신력
