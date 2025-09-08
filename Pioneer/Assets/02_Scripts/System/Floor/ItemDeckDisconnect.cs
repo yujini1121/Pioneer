517cc9c0118a465e158cc0c8adbc4a37b797963e
yujini1121 <yujini1121@naver.com>
2025-09-08 19:24:37 +0900

    2025-09-08 갑판 단절 시스템 수정 중2

--- Pioneer/Assets/02_Scripts/System/Floor/ItemDeckDisconnect.cs
+++ Pioneer/Assets/02_Scripts/System/Floor/ItemDeckDisconnect.cs
@@ -2,34 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 갑판 개개별의 정보
+/// </summary>
+[System.Serializable]
+class DeckInfo
+{
+	public GameObject obj;
+	public bool isConnected;	
+	//public bool IsConnected { get; private set; }
+}
 
-// 갑판 파괴 : BFS로 구현할 것 + 아이템 파괴할 때마다 모든 바닥 검사
+
+/// <summary>
+/// 갑판 파괴 : BFS로 구현할 것 + 아이템 파괴할 때마다 모든 바닥 검사
+/// </summary>
 public class ItemDeckDisconnect : MonoBehaviour
 {
 	public static ItemDeckDisconnect instance;
-	public bool IsConnected { get; private set; }
 
-	[SerializeField] private GameObject mast;
+	[SerializeField] private Transform mast;
 	[SerializeField] private GameObject worldSpace;
+	[SerializeField] private LayerMask deckLayer;
+	[SerializeField] private Vector2 gridSize = new(1, 1);
 
-    private void Awake()
+	private Dictionary<Vector2Int, DeckInfo> decks = new();
+	private Vector2Int[] DIR4 = { Vector2Int.up, Vector2Int.down, Vector2Int.left, Vector2Int.right };
+
+	private void Awake()
 	{
-		if (instance != null && instance != this)
+		deckLayer = LayerMask.GetMask("Platform");
+	}
+
+	private bool IsInLayer(int layer)
+	{
+		return layer == deckLayer;
+	}
+
+	private void InitScan()
+	{
+		decks.Clear();
+		var all = FindObjectsOfType<Transform>(false);
+
+		foreach(var d in all)
 		{
-			Destroy(gameObject);  // 중복 방지
-			return;
+			GameObject go = d.gameObject;
+
+			if (!IsInLayer(go.layer)) continue;
+
+			var 
 		}
-		instance = this;
 	}
 
-
-	public void ScanConnected()
-	{
-
-	}
-
-	public void DestroyDeck()
-    {
-		  
-    }
 }
