a1632180049aa728a9f7f9b4c5fbaa261551f8f0
yujini <yujini1121@naver.com>
2025-11-04 00:12:06 +0900

    Re-import 2

---
+++ Pioneer/Assets/FogOfWar/RenderPipelines/URP/FogOfWarURPFeature.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+namespace FoW
+{
+    public class FogOfWarURPFeature : ScriptableRendererFeature
+    {
+        FogOfWarURPPass _fowPass;
+
+        public override void Create()
+        {
+            _fowPass = new FogOfWarURPPass(RenderPassEvent.BeforeRenderingPostProcessing);
+        }
+
+        public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+        {
+            if (renderingData.cameraData.cameraType != CameraType.Game)
+                return;
+
+            _fowPass.Setup(renderer);
+            renderer.EnqueuePass(_fowPass);
+        }
+    }
+}
---
+++ Pioneer/Assets/FogOfWar/RenderPipelines/URP/Samples/FogOfWarTestURP.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace FoW
+{
+    public class FogOfWarTestURP : FogOfWarTestPlatform
+    {
+        public Volume volume;
+
+        public override void SetCameraTeam(Camera cam, int team)
+        {
+            VolumeProfile profile = volume.sharedProfile;
+            FogOfWarURP fow;
+            if (profile != null && profile.TryGet(out fow))
+                fow.team.value = team;
+        }
+    }
+}
