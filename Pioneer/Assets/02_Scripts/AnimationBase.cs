8099d4d2a4b215984e6771cbd774177d7b2a87c6
yujini <yujini1121@naver.com>
2025-11-10 11:28:43 +0900

    Update

---
+++ Pioneer/Assets/02_Scripts/AnimationBase.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class AnimationBase : MonoBehaviour
+{
+    [Header("공통")]
+    public List<AnimationClip> idle;
+    public List<AnimationClip> run;
+    public List<AnimationClip> fising;
+    public List<AnimationClip> attack;
+
+    [Header("플레이어 전용")]
+    public List<AnimationClip> woodSword;
+    public List<AnimationClip> conchSword;
+    public List<AnimationClip> ironSword;
+
+    [Header("제발잘되게해주세요제발")]
+    public AnimationBase animList;
+}
