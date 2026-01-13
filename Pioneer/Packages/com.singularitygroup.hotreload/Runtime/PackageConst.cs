47d5bfd4bdc70ca42cef6fe46de980de8f9f62ea
yujini <yujini1121@naver.com>
2026-01-13 23:22:55 +0900

    Test Assets

---
+++ Pioneer/Packages/com.singularitygroup.hotreload/Runtime/PackageConst.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace SingularityGroup.HotReload {
+    internal static class PackageConst {
+        //CI changes this property to 'true' for asset store builds.
+        //Don't touch unless you know what you are doing
+        public static bool IsAssetStoreBuild => true;
+
+        
+        public const string Version = "1.13.14";
+        // Never higher than Version
+        // Used for the download
+        public const string ServerVersion = "1.13.11";
+        public const string PackageName = "com.singularitygroup.hotreload";
+        public const string DefaultLocale = Localization.Locale.English;
+        // avoids unreachable code warnings from using const
+        public static string DefaultLocaleField = DefaultLocale;
+        public const string LibraryCachePath = "Library/" + PackageName;
+        public const string ConfigFileName = "hot-reload-config.json";
+    }
+}
