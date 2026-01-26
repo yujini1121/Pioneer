ec0cb2927de8445f939753314752380857fba08a
keios1 <127081597+keios1@users.noreply.github.com>
2026-01-26 14:44:56 +0900

    2025-01-26 흰색으로 스프라이트 이펙트 변경

--- Pioneer/Assets/02_Scripts/Common/CommonBase.cs
+++ Pioneer/Assets/02_Scripts/Common/CommonBase.cs
@@ -10,24 +10,59 @@ public class CommonBase : MonoBehaviour, IBegin
     public bool IsDead = false;
     public GameObject attacker = null;
     public Vector3 dropOffset;
-
     public int CurrentHp => hp;
 
-    /*public virtual void Start()
-    {
-        hp = maxHp;
-    }*/
+    [Header("Hit Effect")]
+    public float hitFlashDuration = 0.1f;
+    public Color hitColor = new Color(3f, 3f, 3f, 1f);
 
-    // ===============
+    // public으로 변경해서 외부에서 설정 가능하게
+    [HideInInspector] public SpriteRenderer spriteRenderer;
+    private Material material;
+    private Coroutine hitFlashCoroutine;
+
     void Start()
     {
         hp = maxHp;
+
+        // SpriteRenderer를 찾아서 초기화
+        if (spriteRenderer == null)
+        {
+            spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        }
+
+        InitializeHitEffect();
     }
-    // ===============
+
+    // 나중에 초기화될 수도 있으니 별도 함수로 분리
+    public void InitializeHitEffect()
+    {
+        if (spriteRenderer == null)
+        {
+            spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        }
+
+        if (spriteRenderer != null)
+        {
+            Debug.Log($"[{gameObject.name}] SpriteRenderer 찾음! 오브젝트: {spriteRenderer.gameObject.name}");
+
+            // Material을 인스턴스화
+            if (material == null)
+            {
+                material = new Material(spriteRenderer.material);
+                spriteRenderer.material = material;
+                Debug.Log($"[{gameObject.name}] Material 생성 완료!");
+            }
+        }
+        else
+        {
+            Debug.LogError($"[{gameObject.name}] SpriteRenderer를 찾을 수 없습니다!");
+        }
+    }
 
     void Update()
     {
-        
+
     }
 
     // 데미지 받는 함수
@@ -37,8 +72,24 @@ public class CommonBase : MonoBehaviour, IBegin
 
         hp -= damage;
         Debug.Log(gameObject.name + "가 " + damage + "의 데미지를 입었습니다! 현재 체력: " + hp);
-
         this.attacker = attacker;
+
+        // Material이 null이면 다시 초기화 시도
+        if (material == null)
+        {
+            InitializeHitEffect();
+        }
+
+        // 피격 효과 실행
+        if (material != null && spriteRenderer != null)
+        {
+            if (hitFlashCoroutine != null)
+            {
+                StopCoroutine(hitFlashCoroutine);
+            }
+
+            hitFlashCoroutine = StartCoroutine(HitFlashEffect());
+        }
 
         if (hp <= 0)
         {
@@ -47,17 +98,41 @@ public class CommonBase : MonoBehaviour, IBegin
         }
     }
 
+    // 피격 효과
+    private IEnumerator HitFlashEffect()
+    {
+        // 밝은 흰색으로 빛나게
+        Color brightWhite = new Color(2.5f, 2.5f, 2.5f, 1f);
+
+        if (material.HasProperty("_Color"))
+            material.SetColor("_Color", brightWhite);
+
+        if (material.HasProperty("_BaseColor"))
+            material.SetColor("_BaseColor", brightWhite);
+
+        material.color = brightWhite;
+
+        yield return new WaitForSeconds(hitFlashDuration);
+
+        // 원래 색상으로 복구
+        if (material.HasProperty("_Color"))
+            material.SetColor("_Color", Color.white);
+
+        if (material.HasProperty("_BaseColor"))
+            material.SetColor("_BaseColor", Color.white);
+
+        material.color = Color.white;
+    }
+
     // 사라졌을때 호출하는 변수 (생명체인 경우 사망했을 때)
     public virtual void WhenDestroy()
     {
         Debug.Log($"{gameObject.name} 오브젝트 파괴");
-
         ItemDropper dropper = GetComponent<ItemDropper>();
         if (dropper != null)
         {
             ItemDropManager.instance.Drop(dropper.GetDroppedItems(), transform.position + dropOffset);
         }
-
         Destroy(gameObject);
     }
 }
